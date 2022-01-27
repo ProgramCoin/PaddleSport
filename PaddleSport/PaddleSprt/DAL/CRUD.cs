96854c96bfcdcf2ef578d25687f76e6a61400eef
NotTheSmartKraken <ahsaad001@gmail.com>
2022-01-27 13:41:24 -0600

    commit my first time project
    commit my first time project.

---
+++ PaddleSport/PaddleSprt/DAL/CRUD.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Data.SqlClient;
+using PaddleSprt.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaddleSprt.DAL
+{
+    public class CRUD
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["PadSport"].ToString());
+
+        public List<Booking> GetBookings()
+        {
+
+            SqlCommand cmd = new SqlCommand("spBooking", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            List<Booking> bookingList = new List<Booking>();
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                Booking booking = new Booking();
+
+                var day = booking.timeSlot.Day;
+                day = int.Parse(dataRow["timeSlot"].ToString());
+
+                booking.playerID = int.Parse(dataRow["playerID"].ToString());
+                booking.name = dataRow["name"].ToString();
+                booking.phone = dataRow["phone"].ToString();
+                booking.Playerprice = int.Parse(dataRow["Playerprice"].ToString());
+                booking.courtID = int.Parse(dataRow["courtID"].ToString());
+                booking.spID = int.Parse(dataRow["spID"].ToString());
+
+                bookingList.Add(booking);
+            }
+            return bookingList;
+        }
+
+        public List<SportCenter> GetSportCenters()
+        {
+            SqlCommand command = new SqlCommand("spSpCenters", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            dataAdapter.UpdateCommand.CommandType = CommandType.StoredProcedure;
+
+            List<SportCenter> sportCenterList = new List<SportCenter>();
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+
+            }
+            return sportCenterList;
+        }
+    }
+}
---
+++ PaddleSport/PaddleSprt/Models/Player.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace PaddleSprt.Models
+{
+    public class Booking
+    {
+        SportCenter sCenter = new SportCenter();
+
+        public int id { get; set; }
+        public int playerID { get; set; }
+
+        [ForeignKey("SportCenterID")]
+        public int? spID { get; set; }
+        public SportCenter SportCenterID { get; set; }
+
+        [ForeignKey("cID")]
+        public int? courtID { get; set; }
+        public Court cID { get; set; }
+        public string name { get; set; }
+        public float Playerprice
+        {
+            get { return sCenter.price; }
+            set { value = Playerprice; }
+        }
+        public string phone { get; set; }
+        public DateTime timeSlot { get; set; }
+
+    }
+
+    public class SportCenter
+    {
+        Court court = new Court();
+        public int id { get; set; }
+        public string name { get; set; }
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public int courtCount
+        {
+            get { return court.count; }
+            set { value = courtCount; }
+        }
+        public string phone { get; set; }
+        public float price { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string zip { get; set; }
+
+    }
+    public class Court
+    {
+        public int id { get; set; }
+
+        [ForeignKey("SportCenterID")]
+        public int? spID { get; set; }
+        public SportCenter SportCenterID { get; set; }
+        public bool availability { get; set; }
+        public int count { get; set; }
+
+    }
+    public class Owner
+    {
+        Court court = new Court();
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class ContextClass : DbContext
+    {
+        public ContextClass(DbContextOptions<ContextClass> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Booking> bookingSet { get; set; }
+        public DbSet<Court> courtSet { get; set; }
+        public DbSet<Owner> ownerSet { get; set; }
+        public DbSet<SportCenter> sportCenterSet { get; set; }
+    }
+}
